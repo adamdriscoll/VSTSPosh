a77066b378632b083acad138e20246d40ab37abe
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-05-24 13:52:42 -0500

    Adding work item and authorization functions

---
+++ VSTS.psm1
@@ -0,0 +1,26 @@
+﻿function Get-TfsAuthorization {
+    param($user, $token)
+
+    $Value = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $token)))
+    ("Basic {0}" -f $value)
+}
+
+function New-TfsWorkItem {
+    param($AccountName, $Project, $User, $Token, $PropertyHashtable, $WorkItemType)
+
+    $authorization = Get-TfsAuthorization -User $user -Token $token
+
+    $Fields = foreach($kvp in $PropertyHashtable)
+    {
+        [PSCustomObject]@{
+            op = 'add'
+            path = '/fields/' + $kvp.Key
+            value = $kvp.value
+        }
+    }
+
+    $Body = $Fields | ConvertTo-Json
+    "https://$AccountName.visualstudio.com/DefaultCollection/$Project/_apis/wit/workitems/$($WorkItemType)?api-version=1.0"
+    Invoke-RestMethod "https://$AccountName.visualstudio.com/DefaultCollection/$Project/_apis/wit/workitems/$($WorkItemType)?api-version=1.0" -Method POST -ContentType 'application/json' -Headers @{Authorization=$authorization} -Body $Body
+}
+
