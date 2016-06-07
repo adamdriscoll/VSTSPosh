d622d37ee93fa5a0dc834c9a1ed483508b8a50c4
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-06-07 09:39:13 -0500

    Adding Get-VstsWorkItemQuery.

--- VSTS.psm1
+++ VSTS.psm1
@@ -56,3 +56,15 @@ function New-VstsWorkItem {
     Invoke-RestMethod "https://$AccountName.visualstudio.com/DefaultCollection/$Project/_apis/wit/workitems/`$$($WorkItemType)?api-version=1.0" -Method PATCH -ContentType 'application/json-patch+json' -Headers @{Authorization=$authorization} -Body $Body
 }
 
+function Get-VstsWorkItemQuery {
+    <#
+    .SYNOPSIS 
+        Returns a list of work item queries from the specified folder.
+    #>
+    param($AccountName, $User, $Token, $Project, $FolderPath)
+
+    $authorization = Get-VstsAuthorization -User $user -Token $token
+
+    Invoke-RestMethod "https://$AccountName.visualstudio.com/DefaultCollection/$Project/_apis/wit/queries?`$depth=1&api-version=1.0" -Method GET -ContentType 'application/json' -Headers @{Authorization=$authorization} 
+}
+
