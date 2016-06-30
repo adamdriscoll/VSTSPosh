1b54d9a19bd794bf37285e3992d2ba468b5c965f
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-06-30 16:25:59 -0500

    Trying Start-FileDownload....

--- PublishModule.ps1
+++ PublishModule.ps1
@@ -1,6 +1,6 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq 'master'-and $env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) 
 {
-	(New-Object System.net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', ('C:\Program Files\PackageManagement\ProviderAssemblies\nuget.exe'))
+	Start-FileDownload 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -FileName 'C:\Program Files\PackageManagement\ProviderAssemblies\nuget.exe'
 	Install-PackageProvider -Name NuGet -Force
 	Start-Sleep 2
 	Publish-Module -NuGetApiKey $env:ApiKey -Path C:\VSTS -Confirm:$False -Verbose 
