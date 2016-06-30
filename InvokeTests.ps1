67fb5170325cef73b99a0e3ae88cfa7c1e08d22c
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-06-30 11:35:45 -0500

    Adding a script instead of inline PowerShell in the YAML for tests.

---
+++ InvokeTests.ps1
@@ -0,0 +1,15 @@
+
+if ($env:APPVEYOR_REPO_BRANCH -eq "master" -and $env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
+{
+    $res = Invoke-Pester -Path ".\" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru -Tag Integration
+    (New-Object "System.Net.WebClient").UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+    if ($res.FailedCount -gt 0) { 
+        throw "$($res.FailedCount) integration tests failed."
+    }
+}
+
+$res = Invoke-Pester -Path ".\" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru -Tag Unit
+(New-Object "System.Net.WebClient").UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+if ($res.FailedCount -gt 0) { 
+	throw "$($res.FailedCount) unit tests failed."
+}
