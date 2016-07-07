302c71fb2bf99bbcdd9f6508b0eca70937124a91
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-07-07 14:50:11 -0500

    Executing ScriptAnalyzer

--- InvokeTests.ps1
+++ InvokeTests.ps1
@@ -1,3 +1,28 @@
+if ($psVersion.Major -ge 5)
+{
+    Write-Verbose -Verbose "Installing PSScriptAnalyzer"
+    $PSScriptAnalyzerModuleName = "PSScriptAnalyzer"
+    Install-PackageProvider -Name NuGet -Force 
+    Install-Module -Name $PSScriptAnalyzerModuleName -Scope CurrentUser -Force 
+    $PSScriptAnalyzerModule = get-module -Name $PSScriptAnalyzerModuleName -ListAvailable
+    if ($PSScriptAnalyzerModule) {
+        # Import the module if it is available
+        $PSScriptAnalyzerModule | Import-Module -Force
+    }
+    else
+    {
+        # Module could not/would not be installed - so warn user that tests will fail.
+        Write-Warning -Message ( @(
+            "The 'PSScriptAnalyzer' module is not installed. "
+            "The 'PowerShell modules scriptanalyzer' Pester test will fail "
+            ) -Join '' )
+    }
+}
+else
+{
+    Write-Verbose -Verbose "Skipping installation of PSScriptAnalyzer since it requires PSVersion 5.0 or greater. Used PSVersion: $($PSVersion)"
+}
+
 
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
 {
---
+++ Unit.tests.ps1
@@ -0,0 +1,18 @@
+﻿Describe "VSTS" -Tags Unit {
+#region ScriptAnalyzer
+    Context 'PSScriptAnalyzer' {
+        It "passes Invoke-ScriptAnalyzer" {
+
+            # Perform PSScriptAnalyzer scan.
+            # Using ErrorAction SilentlyContinue not to cause it to fail due to parse errors caused by unresolved resources.
+            # Many of our examples try to import different modules which may not be present on the machine and PSScriptAnalyzer throws parse exceptions even though examples are valid.
+            # Errors will still be returned as expected.
+            $PSScriptAnalyzerErrors = Invoke-ScriptAnalyzer -path $PSSCriptRoot -Severity Error -Recurse -ErrorAction SilentlyContinue
+            if ($PSScriptAnalyzerErrors -ne $null) {
+                Write-Error "There are PSScriptAnalyzer errors that need to be fixed:`n $PSScriptAnalyzerErrors"
+                Write-Error "For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/psscriptAnalyzer/"
+                $PSScriptAnalyzerErrors.Count | Should Be $null
+            }
+        }     
+    }
+}
