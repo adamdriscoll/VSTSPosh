a34be04abb67432cde32b04fa29f68cf75075838
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-07-19 15:34:24 -0500

    Adding back processes test.

--- Tests/Processes.tests.ps1
+++ Tests/Processes.tests.ps1
@@ -7,11 +7,24 @@ function New-ProjectName {
 }
 
 Import-Module (Join-Path $PSScriptRoot '..\VSTS.psm1') -Force
-
+t 
 Describe "Processes" -Tags "Integration" {
 	$ProjectName = New-ProjectName
 	$Session = New-VSTSSession -AccountName $Account -User $userName -Token $token
 	New-VSTSProject -Session $Session -Name $ProjectName -Wait
+
+	Context "Has default process templates" {		
+ 		It "Returns default process template" {		
+ 			$Process = Get-VstsProcess -Session $Session | Where Name -EQ 'Agile'		
+ 			$Process | Should not be $null		
+ 					
+ 			$Process = Get-VstsProcess -Session $Session | Where Name -EQ 'CMMI'		
+ 			$Process | Should not be $null		
+ 		
+ 			$Process = Get-VstsProcess -Session $Session | Where Name -EQ 'SCRUM'		
+ 			$Process | Should not be $null		
+ 		}		
+ 	}
 
 	Remove-VSTSProject -Session $Session -Name $ProjectName
 }
