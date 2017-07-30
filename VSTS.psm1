c5effbcc5db6a6f68c1efd9705d38cd56f379722
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-07-30 13:54:11 +1200

    Improved WorkItem cmdlets and fixed New-VSTSWorkItem

--- VSTS.psm1
+++ VSTS.psm1
@@ -605,72 +605,158 @@ function Remove-VSTSProject
 <#
 	.SYNOPSIS
 	Get work items from VSTS.
+
+	.PARAMETER Session
+	The session object created by New-VstsSession.
+
+	.PARAMETER Id
+	The Id of a single work item to lookup.
+
+	.PARAMETER Ids
+	The Ids to lookup for multiple work items.
+
+	.PARAMETER AsOf
+	Gets the work items as they existed at this time.
+
+	The date format must be:
+	2014-12-29T20:49:22.103Z
+
+	.PARAMETER Expand
+	Gets work item relationships (work item links, hyperlinks, file attachments, etc.).
 #>
 function Get-VstsWorkItem
 {
-	[CmdletBinding()]
+	[CmdletBinding(DefaultParameterSetName = 'Query')]
 	param
 	(
 		[Parameter(Mandatory = $True)]
 		$Session,
 
-		[Parameter(Mandatory = $True)]
-		[String] $Id
+		[Parameter(Mandatory = $True, ParameterSetName = 'Id')]
+		[String] $Id,
+
+		[Parameter(Mandatory = $True, ParameterSetName = 'Query')]
+		[String[]] $Ids,
+
+		[Parameter(ParameterSetName = 'Query')]
+		[String] $AsOf,
+
+		[Parameter(ParameterSetName = 'Query')]
+		[ValidateSet('All', 'Relations', 'None')]
+		[String] $Expand
 	)
 
-	Invoke-VstsEndpoint -Session $Session -Path 'wit/workitems' -QueryStringParameters @{ids = $id}
+	$path = 'wit/workitems'
+	$additionalInvokeParameters = @{}
+
+	if ($PSCmdlet.ParameterSetName -eq 'Query')
+	{
+		# Convert the Ids into a comma delimited string
+		$PSBoundParameters['Ids'] = ($PSBoundParameters['Ids'] -join ',')
+
+		$additionalInvokeParameters = @{
+			QueryStringParameters    = Get-VstsQueryStringParametersFromBound `
+				-BoundParameters $PSBoundParameters `
+				-ParameterList 'ids', 'asOf'
+			QueryStringExtParameters = Get-VstsQueryStringParametersFromBound `
+				-BoundParameters $PSBoundParameters `
+				-ParameterList 'expand'
+		}
+	}
+	else
+	{
+		if ($PSBoundParameters.ContainsKey('Id'))
+		{
+			$path = ('{0}/{1}' -f $path, $Id)
+		}
+	}
+
+	$result = Invoke-VstsEndpoint `
+		-Session $Session `
+		-Project $Project `
+		-Path $path `
+		@additionalInvokeParameters
+
+	return $result.Value
 }
 
 <#
 	.SYNOPSIS
 	Create new work items in VSTS
+
+	.PARAMETER Session
+	The session object created by New-VstsSession.
+
+	.PARAMETER Project
+	The name of the project to get the policy configuration from.
+
+	.PARAMETER WorkItemType
+	The work item type to create.
+
+	.PARAMETER PropertyHashtable
+	A hash table containing the properties to set for the new
+	work item. Because hash tables to not support '.' in the key
+	all '.' must be replaced with underscore. This will be replaced
+	with a '.' when submitted to the API.
+
+	.EXAMPLE
+	$vstsSession = New-VSTSSession `
+		-AccountName 'myvstsaccount' `
+		-User 'joe.bloggs@fabrikam.com' `
+		-Token 'hi3pxk5usaag6jslczs5bqmlkngvhr3czqyh65jdvlvtt3qkh4ya'
+
+	New-VstsWorkItem `
+		-Session $session `
+		-Project 'FabrikamFiber' `
+		-WorkItemType 'User Story' `
+		-PropertyHashtable @{ System_Title = 'Add support for creating new work item' }
+
+	Creates a new user story in FabrikamFiber project with the
+	title 'Add support for creating new work item'
 #>
 function New-VstsWorkItem
 {
 	param
 	(
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$AccountName,
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$User,
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$Token,
-		[Parameter(Mandatory, ParameterSetname = 'Session')]
+		[Parameter(Mandatory = $True)]
 		$Session,
-		[Parameter(Mandatory)]
-		$Project,
-		[Parameter()]
-		[Hashtable]
-		$PropertyHashtable,
-		[Parameter(Mandatory)]
-		[string]
-		$WorkItemType
+
+		[Parameter(Mandatory = $True)]
+		[String] $Project,
+
+		[Parameter(Mandatory = $True)]
+		[string] $WorkItemType,
+
+		[Parameter(Mandatory = $True)]
+		[Hashtable]	$PropertyHashtable
 	)
 
-	if ($PSCmdlet.ParameterSetName -eq 'Account')
-	{
-		$Session = New-VSTSSession -AccountName $AccountName -User $User -Token $Token
-	}
+	$path = ('wit/workitems/${0}' -f $WorkItemType)
 
-	if ($null -ne $PropertyHashtable)
+	$fields = foreach ($kvp in $PropertyHashtable.GetEnumerator())
 	{
-		$Fields = foreach ($kvp in $PropertyHashtable.GetEnumerator())
-		{
-			[PSCustomObject]@{
-				op    = 'add'
-				path  = '/fields/' + $kvp.Key
-				value = $kvp.value
-			}
+		[PSCustomObject] @{
+			op    = 'add'
+			path  = '/fields/{0}' -f ($kvp.Key.Replace('_','.'))
+			value = $kvp.value
 		}
-
-		$Body = $Fields | ConvertTo-Json
 	}
-	else
+
+	$body = $fields | ConvertTo-Json
+
+	if ($fields.Count -lt 2)
 	{
-		$Body = [String]::Empty
+		$body = ('[{0}]' -f $body)
 	}
 
-	Invoke-VstsEndpoint -Session $Session -Path "wit/workitems/`$$($WorkItemType)" -Method PATCH -Project $Project -Body $Body
+	$result = Invoke-VstsEndpoint `
+		-Session $Session `
+		-Project $Project `
+		-Path $path `
+		-Method 'PATCH' `
+		-Body $body
+
+	return $result.Value
 }
 
 <#
@@ -718,6 +804,34 @@ function Get-VstsWorkItemQuery
 
 <#
 	.SYNOPSIS
+	Gets Git repositories in the specified team project.
+#>
+function Get-VstsGitRepository
+{
+	param
+	(
+		[Parameter(Mandatory, ParameterSetname = 'Account')]
+		$AccountName,
+		[Parameter(Mandatory, ParameterSetname = 'Account')]
+		$User,
+		[Parameter(Mandatory, ParameterSetname = 'Account')]
+		$Token,
+		[Parameter(Mandatory, ParameterSetname = 'Session')]
+		$Session,
+		[Parameter(Mandatory = $true)]$Project
+	)
+
+	if ($PSCmdlet.ParameterSetName -eq 'Account')
+	{
+		$Session = New-VSTSSession -AccountName $AccountName -User $User -Token $Token
+	}
+
+	$Result = Invoke-VstsEndpoint -Session $Session -Project $Project -Path 'git/repositories' -QueryStringParameters @{depth = 1}
+	$Result.Value
+}
+
+<#
+	.SYNOPSIS
 	Creates a new Git repository in the specified team project.
 #>
 function New-VstsGitRepository
@@ -756,34 +870,6 @@ function New-VstsGitRepository
 	} | ConvertTo-Json
 
 	Invoke-VstsEndpoint -Session $Session -Method POST -Path 'git/repositories' -Body $Body
-}
-
-<#
-	.SYNOPSIS
-	Gets Git repositories in the specified team project.
-#>
-function Get-VstsGitRepository
-{
-	param
-	(
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$AccountName,
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$User,
-		[Parameter(Mandatory, ParameterSetname = 'Account')]
-		$Token,
-		[Parameter(Mandatory, ParameterSetname = 'Session')]
-		$Session,
-		[Parameter(Mandatory = $true)]$Project
-	)
-
-	if ($PSCmdlet.ParameterSetName -eq 'Account')
-	{
-		$Session = New-VSTSSession -AccountName $AccountName -User $User -Token $Token
-	}
-
-	$Result = Invoke-VstsEndpoint -Session $Session -Project $Project -Path 'git/repositories' -QueryStringParameters @{depth = 1}
-	$Result.Value
 }
 
 <#
@@ -850,7 +936,8 @@ function Get-VstsCodePolicyConfiguration
 		-Session $Session `
 		-Project $Project `
 		-Path $path `
-		-ApiVersion '2.0-preview.1'
+		-ApiVersion '2.0-preview.1' `
+		@additionalInvokeParameters
 
 	return $result.Value
 }
