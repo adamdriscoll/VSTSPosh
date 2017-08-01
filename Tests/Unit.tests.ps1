bb7359485702e722c4bd54d1f0afba42333195fa
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-08-01 19:41:45 +1200

    Fix PSSA unit tests to scan new Lib files

--- Tests/Unit.tests.ps1
+++ Tests/Unit.tests.ps1
@@ -5,53 +5,58 @@ Describe 'VSTS' -Tags Unit {
 	Context 'PSScriptAnalyzer' {
 		if ($PSVersionTable.PSVersion.Major -ge 5)
 		{
-			$invokeScriptAnalyzerParameters = @{
-				Path        = $modulePath
-				ErrorAction = 'SilentlyContinue'
-				Recurse     = $false
-			}
-
-			Context $invokeScriptAnalyzerParameters.Path {
-				It 'Should pass all error-level PS Script Analyzer rules' {
-					$errorPssaRulesOutput = Invoke-ScriptAnalyzer @invokeScriptAnalyzerParameters -Severity 'Error'
-
-					if ($null -ne $errorPssaRulesOutput)
-					{
-						Write-Warning -Message 'Error-level PSSA rule(s) did not pass.'
-						Write-Warning -Message 'The following PSScriptAnalyzer errors need to be fixed:'
-
-						foreach ($errorPssaRuleOutput in $errorPssaRulesOutput)
-						{
-							Write-Warning -Message "$($errorPssaRuleOutput.ScriptName) (Line $($errorPssaRuleOutput.Line)): $($errorPssaRuleOutput.Message)"
-						}
-
-						Write-Warning -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/PSScriptAnalyzer'
-					}
-
-					$errorPssaRulesOutput | Should Be $null
+			$analyzeFiles = @($modulePath)
+			$analyzeFiles += (Get-ChildItem -Path (Join-Path -Path $moduleRoot -ChildPath 'lib') -Filter '*.ps1').FullName
+			foreach ($analyzeFile in $analyzeFiles)
+			{
+				$invokeScriptAnalyzerParameters = @{
+					Path        = $analyzeFile
+					ErrorAction = 'SilentlyContinue'
+					Recurse     = $false
 				}
 
-				It 'Should pass all warning-level PS Script Analyzer rules' {
-					$requiredPssaRulesOutput = Invoke-ScriptAnalyzer -Path $modulePath
+				Context $invokeScriptAnalyzerParameters.Path {
+					It 'Should pass all error-level PS Script Analyzer rules' {
+						$errorPssaRulesOutput = Invoke-ScriptAnalyzer @invokeScriptAnalyzerParameters -Severity 'Error'
 
-					if ($null -ne $requiredPssaRulesOutput)
-					{
-						Write-Warning -Message 'Required PSSA rule(s) did not pass.'
-						Write-Warning -Message 'The following PSScriptAnalyzer errors need to be fixed:'
-
-						foreach ($requiredPssaRuleOutput in $requiredPssaRulesOutput)
+						if ($null -ne $errorPssaRulesOutput)
 						{
-							Write-Warning -Message "$($requiredPssaRuleOutput.ScriptName) (Line $($requiredPssaRuleOutput.Line)): $($requiredPssaRuleOutput.Message)"
+							Write-Warning -Message 'Error-level PSSA rule(s) did not pass.'
+							Write-Warning -Message 'The following PSScriptAnalyzer errors need to be fixed:'
+
+							foreach ($errorPssaRuleOutput in $errorPssaRulesOutput)
+							{
+								Write-Warning -Message "$($errorPssaRuleOutput.ScriptName) (Line $($errorPssaRuleOutput.Line)): $($errorPssaRuleOutput.Message)"
+							}
+
+							Write-Warning -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/PSScriptAnalyzer'
 						}
 
-						Write-Warning -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/PSScriptAnalyzer'
+						$errorPssaRulesOutput | Should Be $null
 					}
 
-					<#
-						Automatically passing this test until they are passing.
-					#>
-					$requiredPssaRulesOutput = $null
-					$requiredPssaRulesOutput | Should Be $null
+					It 'Should pass all warning-level PS Script Analyzer rules' {
+						$requiredPssaRulesOutput = Invoke-ScriptAnalyzer @invokeScriptAnalyzerParameters -Severity 'Warning'
+
+						if ($null -ne $requiredPssaRulesOutput)
+						{
+							Write-Warning -Message 'Required PSSA rule(s) did not pass.'
+							Write-Warning -Message 'The following PSScriptAnalyzer errors need to be fixed:'
+
+							foreach ($requiredPssaRuleOutput in $requiredPssaRulesOutput)
+							{
+								Write-Warning -Message "$($requiredPssaRuleOutput.ScriptName) (Line $($requiredPssaRuleOutput.Line)): $($requiredPssaRuleOutput.Message)"
+							}
+
+							Write-Warning -Message  'For instructions on how to run PSScriptAnalyzer on your own machine, please go to https://github.com/powershell/PSScriptAnalyzer'
+						}
+
+						<#
+							Automatically passing this test until they are passing.
+						#>
+						$requiredPssaRulesOutput = $null
+						$requiredPssaRulesOutput | Should Be $null
+					}
 				}
 			}
 		}
