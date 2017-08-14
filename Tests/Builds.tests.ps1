179de335331af245988b74e827f1fbaf3de27307
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-08-14 21:53:33 +1200

    Changed integration tests to support longer wait times for project creation

--- Tests/Builds.tests.ps1
+++ Tests/Builds.tests.ps1
@@ -229,7 +229,8 @@ Describe 'Builds' -Tags 'Integration' {
         $projectName = New-ProjectName
         $session = New-VSTSSession -AccountName $account -User $userName -Token $token
         Write-Verbose -Verbose -Message ('Creating VSTS test project {0}' -f $projectName)
-        New-VSTSProject -Session $session -Name $projectName -Wait
+        New-VSTSProject -Session $session -Name $projectName
+        Wait-VSTSProject -Session $session -Name $projectName -Exists -State 'WellFormed' -Attempts 50 -RetryIntervalSec 5
     }
 
     Context 'Has default build queues' {
